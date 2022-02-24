a42147a0e78a573bab545df97d5e7f3fbe17d357
DESKTOP-J6L9H9J\yzlim <yzlim23@gmail.com>
2022-02-25 03:37:40 +0800

    test

--- bot.py
+++ bot.py
@@ -192,11 +192,11 @@ def inline_calendar_handler(update, context):
             # context.bot.send_message(chat_id=update.callback_query.from_user.id,
             #                      text="Date set to " + (rescheduledDateTime.strftime("%d/%m/%Y")),
             #                      reply_markup=ReplyKeyboardRemove())
+            firestore_db.collection(u'orders').document(u'200').update({
+                "numReschedules": str(numReschedules)
+            })
             # firestore_db.collection(u'orders').document(u'200').update({
-            #     "numReschedules": str(numReschedules)
-            # })
-            # firestore_db.collection(u'orders').document(u'200').update({
-            #     { "deliveryDate": rescheduledDateTime.strftime("%d/%m/%Y") }
+            #     { "deliveryDate": rescheduledDateTime.strftime("%d/%m/%Y")}
             # })
             context.bot.send_message(chat_id=get_chat_id(update, context), text=f"Your delivery has been rescheduled to " + (rescheduledDateTime.strftime("%d/%m/%Y"))
                                      ,reply_markup=ReplyKeyboardRemove())
