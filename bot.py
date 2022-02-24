85e385e55dd0963752ec0342cca615307c3d1141
Taufiq Bin Abdul Rahman <taufiq.b.ar@gmail.com>
2022-02-24 19:33:29 +0800

    Callback (#5)

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import threading
 import json
 from telegram.ext import Updater, CommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-# from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 import firebase_admin
 from firebase_admin import firestore
@@ -26,7 +25,6 @@ PORT = int(os.environ.get('PORT', '8443'))
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 APP_NAME = os.getenv("APP_NAME")
 
-app = Flask(__name__)
 
 def get_chat_id(update, context):
     chat_id = -1
@@ -43,25 +41,98 @@ def get_chat_id(update, context):
 
     return chat_id
 
+
 def get_update_keyboard():
     options = []
-    options.append(InlineKeyboardButton(text='View Plan', callback_data='1'))
-    options.append(InlineKeyboardButton(text='Upgrade Plan', callback_data='2'))
-    options.append(InlineKeyboardButton(text='Set Delivery Date', callback_data='3'))
-    options.append(InlineKeyboardButton(text='Reschedule Delivery Date', callback_data='4'))
+    options.append(InlineKeyboardButton(text='View Orders', callback_data='view_orders_action'))
+    options.append(InlineKeyboardButton(text='Upgrade Plan', callback_data='upgrade_plan_action_'))
+    options.append(InlineKeyboardButton(text='Set Delivery Date', callback_data='set_date_action'))
+    options.append(InlineKeyboardButton(text='Reschedule Delivery Date', callback_data='reschedule_action_'))
     keyboard = InlineKeyboardMarkup([options])
     return keyboard
+
+
+# Handles the callback functions
+def query_handler(update, context):
+    # Here, we'll have access to the user's answer
+    query = update.callback_query
+    query.answer()
+
+    # Change your comparisons depending on what you chose as 'callback_data'
+    if query.data == 'view_orders_action':
+        view_orders(update, context)
+    elif 'upgrade_plan_action_' in query.data:
+        upgrade_plan(update, context)
+    elif "order-id-" in query.data:
+        order_id = query.data[9:]
+        get_order(update, context, order_id)
+    return
+
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    # doc = firestore_db.collection(u'users').document(u'1').get()
-    # doc_dict = doc.to_dict()
-    # name = doc_dict['name']
-    # update.message.reply_text(name)
-    context.bot.send_message(chat_id=get_chat_id(update, context), text='Welcome to Ninja Scheduler! How can I help you with your delivery today?')
+    context.bot.send_message(chat_id=get_chat_id(update, context),
+                             text='Welcome to Ninja Scheduler! How can I help you with your delivery today?',
+                             reply_markup=get_update_keyboard())
     # input from text message
     
+
+def convert_order_to_button(order_id):
+    return InlineKeyboardButton(text=str(order_id), callback_data="order-id-" + str(order_id))
+
+
+def get_orders_keyboard(update, context):
+    users_collection = firestore_db.collection(u'users')
+    doc = users_collection.document(update.callback_query.message.chat.username).get()
+    doc_dict = doc.to_dict()
+    orders = doc_dict['orders']
+    options = list(map(convert_order_to_button, orders))
+    keyboard = InlineKeyboardMarkup([options])
+    return keyboard
+
+
+# Retrieves the orders
+def view_orders(update, context):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text='Which order do you want to view?',
+                                 reply_markup=get_orders_keyboard(update, context))
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve orders.")
+
+
+def get_order_keyboard(order_id):
+    options = [InlineKeyboardButton(text='Upgrade Plan', callback_data='upgrade_plan_action_' + order_id),
+               InlineKeyboardButton(text='Reschedule Order', callback_data='reschedule_action_' + order_id)]
+    keyboard = InlineKeyboardMarkup([options])
+    return keyboard
+
+
+def get_order(update, context, order_id):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        order = firestore_db.collection(u'orders').document(order_id).get();
+        order_dict = order.to_dict()
+        date_time = order_dict["deliveryDate"].strftime("%m/%d/%Y, %H:%M:%S")
+        del_type = order_dict["deliveryType"]
+        num_res = order_dict["numReschedules"]
+        text = "Your order {} is due to arrive on {}. \nThe current delivery type for this order is: {}.\n You have {" \
+               "} reschedules left.\n What do you want to do? "
+        formatted_text = text.format(order_id, date_time, del_type, str(2 - int(num_res)))
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text=formatted_text,
+                                 reply_markup=get_order_keyboard(order_id))
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve order.")
+
 
 def viewType(update, context):
     # retrieve the deliveryType from firestore
@@ -194,6 +265,7 @@ def upgradePlan(update, context):
         else:
             update.message.reply_text("You are already at the highest delivery tier! Do /reschedule if you wish to reschedule your package.")
 
+
 def upgrade_from_standard(update, del_type, order_ref):
     if del_type == "standard":
         update.message.reply_text("You are already at this tier.")
@@ -203,7 +275,8 @@ def upgrade_from_standard(update, del_type, order_ref):
             "deliveryType": del_type
         })
         update.message.reply_text("Successfully upgraded to " + del_type + "!")
-    
+
+
 def upgrade_from_express(update, del_type, order_ref):
     if del_type == "standard" or del_type == "express":
         update.message.reply_text("You are already at/above this tier. Do /reschedule if you wish to reschedule your package.")
@@ -220,8 +293,6 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-
-
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
@@ -234,9 +305,8 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("upgrade", upgradePlan))
-    dp.add_handler(CommandHandler("view", viewType))
-    dp.add_handler(CommandHandler("reschedule", reschedule))
+    dp.add_handler(CommandHandler("upgrade", upgrade_plan))
+    dp.add_handler(CallbackQueryHandler(query_handler))
 
     # log all errors
     dp.add_error_handler(error)
