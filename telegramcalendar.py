a7f3c803da4d272d3533c21730aee20f6e5045f8
DESKTOP-J6L9H9J\yzlim <yzlim23@gmail.com>
2022-02-25 01:45:06 +0800

    test

--- telegramcalendar.py
+++ telegramcalendar.py
@@ -70,12 +70,12 @@ def process_calendar_selection(update,context):
     if action == "IGNORE":
         context.bot.answer_callback_query(callback_query_id= query.id)
     elif action == "DAY":
-        print('day selected')
         context.bot.edit_message_text(text=query.message.text,
             chat_id=query.message.chat_id,
             message_id=query.message.message_id
             )
         ret_data = True,datetime.datetime(int(year),int(month),int(day))
+        print(ret_data)
     elif action == "PREV-MONTH":
         pre = curr - datetime.timedelta(days=1)
         context.bot.edit_message_text(text=query.message.text,
