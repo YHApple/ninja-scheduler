b4931ff59202be06d56103df5c23fdde18e73067
tyanhan <tayyanhanw@gmail.com>
2022-02-24 21:15:31 +0800

    Add upgrade tier

--- bot.py
+++ bot.py
@@ -51,7 +51,6 @@ def get_update_keyboard():
     keyboard = InlineKeyboardMarkup([options])
     return keyboard
 
-
 # Handles the callback functions
 def query_handler(update, context):
     # Here, we'll have access to the user's answer
@@ -63,9 +62,21 @@ def query_handler(update, context):
         view_orders(update, context)
     elif 'upgrade_plan_action_' in query.data:
         upgrade_plan(update, context)
-    elif "order-id-" in query.data:
-        order_id = query.data[9:]
+    elif "view-order-id-" in query.data:
+        order_id = query.data[14:]
         get_order(update, context, order_id)
+    elif "upgrade-order-id-" in query.data:
+        order_id = query.data[17:]
+        upgrade_order(update, context, order_id)
+    elif "_to_express_tier" in query.data:
+        order_id = query.data[8:]
+        upgrade_to_express(update, context, order_id)
+    elif "_to_timeslot_tier" in query.data:
+        order_id = query.data[8:]
+        upgrade_to_timeslot(update, context, order_id)
+    elif "_to_14day_tier" in query.data:
+        order_id = query.data[8:]
+        upgrade_to_14day(update, context, order_id)
     return
 
 
@@ -78,19 +89,18 @@ def start(update, context):
     # input from text message
     
 
-def convert_order_to_button(order_id):
-    return InlineKeyboardButton(text=str(order_id), callback_data="order-id-" + str(order_id))
+def convert_order_to_button(order_id, action):
+    return InlineKeyboardButton(text=str(order_id), callback_data= action + "-order-id-" + str(order_id))
 
 
-def get_orders_keyboard(update, context):
+def get_orders_keyboard(update, context, action):
     users_collection = firestore_db.collection(u'users')
     doc = users_collection.document(update.callback_query.message.chat.username).get()
     doc_dict = doc.to_dict()
     orders = doc_dict['orders']
-    options = list(map(convert_order_to_button, orders))
+    options = list(map(convert_order_to_button, orders, (action,) * len(orders)))
     keyboard = InlineKeyboardMarkup([options])
     return keyboard
-
 
 # Retrieves the orders
 def view_orders(update, context):
@@ -99,7 +109,7 @@ def view_orders(update, context):
         time.sleep(1)
         context.bot.send_message(chat_id=get_chat_id(update, context),
                                  text='Which order do you want to view?',
-                                 reply_markup=get_orders_keyboard(update, context))
+                                 reply_markup=get_orders_keyboard(update, context, "view"))
     except Exception as e:
         print(e)
         context.bot.send_message(chat_id=get_chat_id(update, context),
@@ -107,7 +117,7 @@ def view_orders(update, context):
 
 
 def get_order_keyboard(order_id):
-    options = [InlineKeyboardButton(text='Upgrade Plan', callback_data='upgrade_plan_action_' + order_id),
+    options = [InlineKeyboardButton(text='Upgrade Plan', callback_data='upgrade-order-id_' + order_id),
                InlineKeyboardButton(text='Reschedule Order', callback_data='reschedule_action_' + order_id)]
     keyboard = InlineKeyboardMarkup([options])
     return keyboard
@@ -244,48 +254,106 @@ def reschedule(update, context):
                 context.bot.send_message(chat_id=get_chat_id(update, context), text=f"Your delivery has been rescheduled to {rescheduleDateTime}")
 
 def upgradePlan(update, context):
-    # Check if command usage is correct
-    if update.message.text.strip() == '/upgrade': 
-        update.message.reply_text("Please specify the type of plan you want to upgrade to! \n Usage:/upgrade [orderId] [deliveryType] \n eg. /upgrade 100 time-slot")
-    else: 
-        # Obtain arguments of the command
-        command = update.message.text.split(" ")
-        order_id = command[1]
-        delivery_type = command[2]
-        
-        # TODO: insert order_id into the database read
-        order_ref = firestore_db.collection(u'orders').document(u'100')
-        order_doc = order_ref.get()
-        order_dict = order_doc.to_dict()
-        current_type = order_dict["deliveryType"]
-        if current_type == "standard":
-            upgrade_from_standard(update, delivery_type, order_ref)
-        elif current_type == "express":
-            upgrade_from_express(update, delivery_type, order_ref)
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text='Which order do you want to upgrade?',
+                                 reply_markup=get_orders_keyboard(update, context, "upgrade"))
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve orders.")
+
+def upgrade_order(update, context, order_id):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        order = firestore_db.collection(u'orders').document(order_id).get();
+        order_dict = order.to_dict()
+        date_time = order_dict["deliveryDate"].strftime("%m/%d/%Y, %H:%M:%S")
+        del_type = order_dict["deliveryType"]
+        num_res = order_dict["numReschedules"]
+        text = "Your order {} is due to arrive on {}. \nThe current delivery type for this order is: {}.\n You have {" \
+               "} reschedules left.\n What plan would you like to upgrade to? "
+        formatted_text = text.format(order_id, date_time, del_type, str(2 - int(num_res)))
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text=formatted_text,
+                                 reply_markup=get_upgrade_keyboard(order_id))
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve order.")
+
+def get_upgrade_keyboard(order_id):
+    options = []
+    options.append(InlineKeyboardButton(text='Express Tier', callback_data="upgrade-" + order_id + "_to_express_tier"))
+    options.append(InlineKeyboardButton(text='TimeSlot Tier', callback_data="upgrade-" + order_id + "_to_timeslot_tier"))
+    options.append(InlineKeyboardButton(text='14 Day Tier', callback_data="upgrade-" + order_id + "_to_14day_tier"))
+    keyboard = InlineKeyboardMarkup([options])
+    return keyboard
+
+def upgrade_to_express(update, context, order_id):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        order = firestore_db.collection(u'orders').document(order_id).get();
+        order_dict = order.to_dict()
+        del_type = order_dict["deliveryType"]
+        if "express" in del_type:
+            update.message.reply_text("You are already at this tier. Do /reschedule if you wish to reschedule your package.")
+        elif "timeslot" in del_type:
+            update.message.reply_text("You are already at a higher tier. Do /reschedule if you wish to reschedule your package.")
         else:
-            update.message.reply_text("You are already at the highest delivery tier! Do /reschedule if you wish to reschedule your package.")
+            # stripe API
+            order_id.update({
+                "deliveryType": "express"
+            })
+            update.message.reply_text("Successfully upgraded to express tier!")
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve order.")
 
+def upgrade_to_timeslot(update, context, order_id):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        order = firestore_db.collection(u'orders').document(order_id).get();
+        order_dict = order.to_dict()
+        del_type = order_dict["deliveryType"]
+        if "timeslot" in del_type:
+            update.message.reply_text("You are already at this tier. Do /reschedule if you wish to reschedule your package.")
+        else:
+            # stripe API
+            order_id.update({
+                "deliveryType": "timeslot"
+            })
+            update.message.reply_text("Successfully upgraded to timeslot tier!")
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve order.")
 
-def upgrade_from_standard(update, del_type, order_ref):
-    if del_type == "standard":
-        update.message.reply_text("You are already at this tier.")
-    elif del_type == "express" or  del_type == "time-slot":
-        # stripe API
-        order_ref.update({
-            "deliveryType": del_type
-        })
-        update.message.reply_text("Successfully upgraded to " + del_type + "!")
-
-
-def upgrade_from_express(update, del_type, order_ref):
-    if del_type == "standard" or del_type == "express":
-        update.message.reply_text("You are already at/above this tier. Do /reschedule if you wish to reschedule your package.")
-    elif del_type == "time-slot":
-        # stripe API
-        order_ref.update({
-            "deliveryType": del_type
-        })
-        update.message.reply_text("Successfully upgraded to " + del_type + "!")
+def upgrade_to_14day(update, context, order_id):
+    try:
+        context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=ChatAction.TYPING, timeout=1)
+        time.sleep(1)
+        order = firestore_db.collection(u'orders').document(order_id).get();
+        order_dict = order.to_dict()
+        del_type = order_dict["deliveryType"]
+        if "14day" in del_type:
+            update.message.reply_text("You are already at this tier. Do /reschedule if you wish to reschedule your package.")
+        else:
+            # stripe API
+            order_id.update({
+                "deliveryType": "14day " + del_type
+            })
+            update.message.reply_text("Successfully upgraded to 14day " + del_type + " tier!")
+    except Exception as e:
+        print(e)
+        context.bot.send_message(chat_id=get_chat_id(update, context),
+                                 text="Sorry, unable to retrieve order.")
 
 
 def error(update, context):
