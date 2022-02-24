85f224407674d1a61c943fad272a38bf66272d6d
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2022-02-24 11:33:09 +0800

    test telegram buttons

--- bot.py
+++ bot.py
@@ -77,11 +77,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=PORT,
-    #                       url_path=TOKEN)
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
 
-    # updater.bot.set_webhook(APP_NAME + TOKEN)
+    updater.bot.set_webhook(APP_NAME + TOKEN)
 
     # # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # # SIGTERM or SIGABRT. This should be used most of the time, since
