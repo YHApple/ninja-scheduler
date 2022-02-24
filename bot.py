e00c0aca50029d0ba8f829f2f2ad3a8fd2167e18
tyanhan <tayyanhanw@gmail.com>
2022-02-24 18:14:20 +0800

    Fix bug

--- bot.py
+++ bot.py
@@ -234,7 +234,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("upgrade", upgradePlan))
     dp.add_handler(CommandHandler("view", viewType))
-    dp.add_handler(CommandHandler("reschedue", reschedule))
+    dp.add_handler(CommandHandler("reschedule", reschedule))
 
     # log all errors
     dp.add_error_handler(error)
